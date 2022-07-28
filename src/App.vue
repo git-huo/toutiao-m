62260e23fa46e762896c4bee355556488cc2b672
霍毫 <huohao@Macintosh.local>
2022-07-28 10:34:03 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    黑马头条 <span class="toutiao toutiao-wode"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
